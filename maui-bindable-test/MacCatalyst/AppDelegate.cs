e3625a439eab3c99a4485a523230fdb8308b0d2b
Matt Goldman <matt.goldman@outlook.com>
2021-06-21 06:48:11 +1000

    added maui project

---
+++ maui-bindable-test/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+using Microsoft.Maui;
+
+namespace maui_bindable_test
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate<Startup>
+	{
+	}
+}
---
+++ maui-bindable-test/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Hosting;
+
+namespace maui_bindable_test
+{
+	public class Startup : IStartup
+	{
+		public void Configure(IAppHostBuilder appBuilder)
+		{
+			appBuilder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
+		}
+	}
+}
